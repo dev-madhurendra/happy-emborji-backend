f66db36eea88cd0613238c3967e03290084d6028
dev-madhurendra <dev.madhurendra@gmail.com>
2025-10-30 00:57:49 +0530

    feat : added fallback

--- app.js
+++ app.js
@@ -5,14 +5,26 @@ import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
 import connectDB from "./db.js";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // MongoDB setup
 connectDB();
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 
 // Cloudinary config
 cloudinary.config({
@@ -35,14 +47,6 @@ const ProductSchema = new mongoose.Schema({
   images: [String],
 });
 const Product = mongoose.model("Product", ProductSchema);
-
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "dist"))); // your build folder
-
-// ✅ Catch-all route for React Router
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
 
 // Upload endpoint
 app.post("/api/addProduct", upload.fields([
